04b98237153a782e917bceb103b178b0fd6a12c6
Sam Weiss <sweiss@cct.ie>
2023-03-07 21:33:59 +0000

    Print Hello Git World
    Added sout and removed some comments.

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gittest;
 
 /**
- *
  * @author sweis
  */
 public class GitTest {
 
     /**
      * @param args the command line arguments
+     * 
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Git World");
     }
     
 }
